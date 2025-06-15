2fb17ccb4dd9cfe78980d3c83cfd40972f1b94da
M-Munim <muhammadmunimoff330@gmail.com>
2025-06-15 18:04:10 +0500

    user, task schema || auth(login, signup, get user, profile image upload)

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+// const userRoutes = require('./routes/userRoutes');
+// const taskRoutes = require('./routes/taskRoutes');
+// const reportRoutes = require('./routes/reportRoutes');
 
 const app = express();
 
@@ -15,8 +20,11 @@ app.use(cors({
 // Middlewares
 app.use(express.json());
 
+// Connect to MongoDB
+connectDB();
+
 // Routes
-// app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 // app.use('/api/users', userRoutes);
 // app.use('/api/tasks', taskRoutes);
 // app.use('/api/reports', reportRoutes);
