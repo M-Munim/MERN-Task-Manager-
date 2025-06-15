9dadd666d339ae3a19ef02697feffa64aec9604b
M-Munim <muhammadmunimoff330@gmail.com>
2025-06-15 13:55:11 +0500

    routing and BE setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Admin/Dashboard.jsx'
+import Login from './pages/Auth/Login.jsx'
+import SignUp from './pages/Auth/SignUp.jsx'
+import ManageTasks from './pages/Admin/ManageTasks.jsx'
+import CreateTask from './pages/Admin/CreateTask.jsx'
+import ManageUsers from './pages/Admin/ManageUsers.jsx'
+import UserDashboard from './pages/User/UserDashboard.jsx'
+import MyTasks from './pages/User/MyTasks.jsx'
+import ViewTaskDetails from './pages/User/ViewTaskDetails.jsx'
+import PrivateRoute from './routes/PrivateRoute.jsx'
 
 const App = () => {
   return (
-    <div className='bg-red-100'>
-      app
+    <div className='bg-red-100 '>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signUp' element={<SignUp />} />
+
+          {/* Admin Routes */}
+          <Route element={<PrivateRoute allowedRoles={['admin']} />} >
+            <Route path='/admin/dashboard' element={<Dashboard />} />
+            <Route path='/admin/tasks' element={<ManageTasks />} />
+            <Route path='/admin/create-task' element={<CreateTask />} />
+            <Route path='/admin/users' element={<ManageUsers />} />
+          </Route>
+
+          {/* user Routes */}
+          <Route element={<PrivateRoute allowedRoles={['admin']} />} >
+            <Route path='/user/dashboard' element={<UserDashboard />} />
+            <Route path='/user/my-tasks' element={<MyTasks />} />
+            <Route path='/user/task-details/:id' element={<ViewTaskDetails />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
