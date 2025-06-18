a568ceb2a14024f9f7493e7080b6f0302540d316
M-Munim <muhammadmunimoff330@gmail.com>
2025-06-18 23:08:10 +0500

    task management api and creating report generation apis

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ const cors = require('cors');
 const path = require('path');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-// const userRoutes = require('./routes/userRoutes');
-// const taskRoutes = require('./routes/taskRoutes');
-// const reportRoutes = require('./routes/reportRoutes');
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const reportRoutes = require('./routes/reportRoutes');
 
 const app = express();
 
@@ -25,9 +25,9 @@ connectDB();
 
 // Routes
 app.use('/api/auth', authRoutes);
-// app.use('/api/users', userRoutes);
-// app.use('/api/tasks', taskRoutes);
-// app.use('/api/reports', reportRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/tasks', taskRoutes);
+app.use('/api/reports', reportRoutes);
 
 
 // Server
