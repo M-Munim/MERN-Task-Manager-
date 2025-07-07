f9d9d9746d132f1f1427cd4756d7e891bb245cb8
M-Munim <muhammadmunimoff330@gmail.com>
2025-07-07 10:54:20 +0500

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,7 @@ const reportRoutes = require('./routes/reportRoutes');
 const app = express();
 
 // Middleware to handle CORS
-app.use(cors({
-    origin: "*",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 // Middlewares
 app.use(express.json());
